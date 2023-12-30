a024f5a1ebd2b506a18d3b6e6d6f213a761392c6
barbarajunq131 <150540002+barbarajunq131@users.noreply.github.com>
2023-12-29 21:46:31 -0300

    Update add.py

--- add.py
+++ add.py
@@ -15,21 +15,24 @@ c.execute('''
     )
 ''')
 # Dados fictícios para a base de dados (simulação)
+# Dados fictícios para a base de dados (simulação)
 referencia = [
-    ("DAVIS, ANGELA", " Mulheres, Raça e Classe", "Capitulo 3 Classe e raça no início da campanha pelos direitos das mulheres", "9a aula"),
-    ("MUNANGA, KABENGELE", "Rediscutindo a mestiçagem no Brasil: identidade nacional versus identidade negra", "(Capítulo I: Conceito e História da Mestiçagem, páginas 17 a 47)", "2a aula"),
-    ("CARNEIRO, SUELI", "O mito da democracia racial", " ", "2a. aula"),
-    ("CORRÊA, MARIZA", "O mistério dos orixás e das bonecas: raça e gênero na antropologia brasileira", "pp. 233-265", "3a aula"),
+    ("DAVIS, ANGELA", "Mulheres, Raça e Classe", "Capitulo 3 Classe e raça no início da campanha pelos direitos das mulheres", "9"),
+    ("MUNANGA, KABENGELE", "Rediscutindo a mestiçagem no Brasil: identidade nacional versus identidade negra", "(Capítulo I: Conceito e História da Mestiçagem, páginas 17 a 47)", "2"),
+    ("CARNEIRO, SUELI", "O mito da democracia racial", " ", "2"),
+    ("CORRÊA, MARIZA", "O mistério dos orixás e das bonecas: raça e gênero na antropologia brasileira", "pp. 233-265", "3"),
 ]
 
-# Adicionar a coluna 'aula' se não existir
-try:
-    c.execute("ALTER TABLE referencia ADD COLUMN aula TEXT")
-    conn.commit()
-except sqlite3.OperationalError as e:
-    pass
 
+# Inserir dados na tabela
+c.executemany('''
+    INSERT INTO referencia (autora, referencia, capitulo, aula) VALUES (?, ?, ?, ?)
+''', referencia)
+
+# Commit para salvar as alterações
+conn.commit()
+
+# Fechar a conexão
 conn.close()
 
 print("Banco de dados criado com sucesso.")
-
